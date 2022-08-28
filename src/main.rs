2ad631e96037b3a451aead47f963e92a747c0f86
Marius Pardo <pardomarius@gmail.com>
2022-08-28 13:18:24 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,148 @@
-use std::thread;
+use std::fmt::{Display, Formatter};
 use std::time::Duration;
 
 use log::{error, info, warn};
 use reqwest::blocking::Client;
-use reqwest::Error;
 
-fn fetch_ip(client: &Client) -> Result<String, Error> {
-    let r = client.get("https://domains.google.com/checkip").send()?.text()?;
-    Ok(r)
+enum Error {
+    FetchIp(reqwest::Error),
+    PostIp(reqwest::Error),
 }
 
-fn post_ddns(client: &Client, auth: &str, host: &str, ip: &str) -> Result<String, Error> {
-    client.post(format!("https://domains.google.com/nic/update?hostname={}&myip={}", host, ip))
-        .header("Authorization", format!("Basic {}", auth))
-        .header("Content-length", 0)
-        .send()?
-        .text()
-}
-
-fn do_work(last_ip: &mut Option<String>, username: &str, password: &str, host: &str) -> Result<(), Error> {
-    let client = Client::builder()
-        .tcp_keepalive(Duration::from_secs(10))
-        .timeout(Duration::from_secs(5))
-        .user_agent("GoogleDDNS/0.1.0")
-        .build()?;
-    if let Ok(ip) = &fetch_ip(&client) {
-        let do_post_ddns = if let Some(last_ip) = last_ip.as_ref() {
-            if ip.eq(last_ip) {
-                info!("Same ip, don't request: {}", ip);
-                false
-            } else {
-                true
-            }
-        } else {
-            *last_ip = Some(ip.clone());
-            true
-        };
-        if do_post_ddns {
-            let encoded = base64::encode(format!("{}:{}", username, password));
-            match post_ddns(&client, &encoded, &host, &ip) {
-                Ok(res) => info!("{}", res),
-                Err(e) => error!("{}", e),
-            }
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::FetchIp(e) => { write!(f, "Ip fetching error : {e}") }
+            Error::PostIp(e) => { write!(f, "Ip posting error : {e}") }
         }
     }
-    Ok(())
 }
 
-fn main() {
+type Result<T> = std::result::Result<T, Error>;
+
+trait HandleError {
+    fn handle_error(self) -> Self;
+}
+
+impl<T> HandleError for Result<T> {
+    fn handle_error(self) -> Self {
+        match &self {
+            Ok(_) => {}
+            Err(e) => {
+                error!("{e}");
+            }
+        }
+        self
+    }
+}
+
+struct ConnectionData {
+    auth: String,
+    host: String,
+}
+
+struct Context {
+    data: ConnectionData,
+    client: Client,
+}
+
+fn fetch_ip(context: &Context) -> Result<String> {
+    context.client.get("https://domains.google.com/checkip")
+        .send().map_err(|e| Error::FetchIp(e))?
+        .text()
+        .map_err(|e| Error::FetchIp(e))
+}
+
+fn post_ddns(context: &Context, ip: &str) -> Result<String> {
+    context.client.post(format!("https://domains.google.com/nic/update?hostname={}&myip={}", context.data.host, ip))
+        .header("Authorization", format!("Basic {}", context.data.auth))
+        .header("Content-length", 0)
+        .send().map_err(|e| Error::FetchIp(e))?
+        .text()
+        .map_err(|e| Error::PostIp(e))
+}
+
+fn do_loop(context: &Context) {
+    let wait_interval = get_wait_interval();
+    let mut last_ip = "".to_string();
+
+    loop {
+        match fetch_ip(context) {
+            Ok(ip) => {
+                info!("Ip is {ip}");
+                info!("Last ip was {last_ip}");
+
+                if last_ip != ip {
+                    info!("Different ip, posting ip to ddns");
+                    match post_ddns(context, &ip) {
+                        Ok(res) => info!("{res}"),
+                        Err(e) => error!("Failed to post ip : {e}"),
+                    }
+                    last_ip = ip;
+                } else {
+                    info!("Same ip, skipping ddns post")
+                }
+            }
+            Err(e) => {
+                error!("{e}");
+            }
+        };
+
+        std::thread::sleep(wait_interval)
+    }
+}
+
+fn setup_logger() {
     if let Err(_e) = std::env::var("RUST_LOG") {
         std::env::set_var("RUST_LOG", "info");
     }
     pretty_env_logger::init();
+}
 
-
+fn get_context() -> Context {
     let username = std::env::var("DDNS_USERNAME").expect("DDNS_USERNAME env variable is not set");
     let password = std::env::var("DDNS_PASSWORD").expect("DDNS_PASSWORD env variable is not set");
+
+    let auth = base64::encode(format!("{}:{}", username, password));
+
     let host = std::env::var("DDNS_HOST").expect("DDNS_HOST env variable is not set");
+
+
+    let client = Client::builder()
+        .timeout(Duration::from_secs(10))
+        .user_agent("GoogleDDNS/0.1.0")
+        .https_only(true)
+        .build()
+        .expect("Could not build http client");
+
+    Context {
+        data: ConnectionData {
+            auth,
+            host,
+        },
+        client,
+    }
+}
+
+fn get_wait_interval() -> Duration {
     let minutes_interval = match std::env::var("DDNS_MINUTES_INTERVAL") {
         Ok(i) => i.parse().unwrap_or_else(|_| {
             warn!("Invalid DDNS_MINUTES_INTERVAL env variable : {}, defaulting to 30 minutes", i);
             30
         }),
         Err(_) => {
-            info!("DDNS_INTERVAL env variable is not set, defaulting to 30 minutes");
+            warn!("DDNS_INTERVAL env variable is not set, defaulting to 30 minutes");
             30
         }
     };
 
-    info!("Using interval : {} minutes", minutes_interval);
+    info!("Using wait interval of {minutes_interval} minute(s)");
 
-    let mut last_ip: Option<String> = None;
+    Duration::from_secs(minutes_interval * 60)
+}
 
-    loop {
-        if let Err(e) = do_work(&mut last_ip, &username, &password, &host) {
-            error!("{}", e);
-        }
-        thread::sleep(Duration::from_secs(minutes_interval * 60));
-    }
+fn main() {
+    setup_logger();
+    let context = get_context();
+    do_loop(&context);
 }
